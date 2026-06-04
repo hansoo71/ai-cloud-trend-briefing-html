6030333480424d37043a044c70933cc283bc9bca
Hansoo KIm <hansoo71@gmail.com>
2026-06-05 08:19:50 +0900

    Sync docs/app.js

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(function(a){a.addEventListener('click',function(e){var id=a.getAttribute('href').slice(1);var el=document.getElementById(id);if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'});}});});
+document.querySelectorAll('a[href^="#"]').forEach(function(a){a.addEventListener('click',function(e){var t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth',block:'start'});}});});
