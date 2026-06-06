6cdf8d50924436565ee8c3757c99faf633184b6c
Hansoo KIm <hansoo71@gmail.com>
2026-06-07 08:08:12 +0900

    Sync 2026-06-07 AI cloud briefing via API

---
+++ docs/20260607/app.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(function(a){a.addEventListener('click',function(e){var t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth',block:'start'});}});});
